8f1755dbac1ca3c1b00c6e8e4eebda94e4863d5b
ToxicTree <zoi009900@gmail.com>
2016-09-30 11:55:16 +0200

    Default Settings when creating users

---
+++ config/returbutiken.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Returbutiken default settings, only used when setting up the database.
+ */
+
+return [
+    'settings'=> [
+        'users' => [
+            'administrator' => [
+                'username' => 'admin',
+                'password' => 'admin'
+            ]
+        ],
+    ]
+];
