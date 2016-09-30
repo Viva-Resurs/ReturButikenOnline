0497781fc1707f9bf817f817fc32337f9b25f5ae
ToxicTree <zoi009900@gmail.com>
2016-09-30 11:56:06 +0200

    DatabaseSeeder: Create users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Role;
+use Illuminate\Database\Eloquent\Model;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(RolesTablesSeeder::class);
+        $this->call(UsersTableSeeder::class);
+    }
+}
+
+class RolesTablesSeeder extends Seeder {
+    public function run() {
+        DB::table('roles')->delete();
+
+        // Admin
+        $admin = Role::create([
+            'name'  => 'admin',
+            'label' => 'auth.administrator',
+        ]);
+    }
+}
+
+class UsersTableSeeder extends Seeder {
+    public function run() {
+        DB::table('users')->delete();
+
+        $admin = User::create([
+            'name'     => config('returbutiken.settings.users.administrator.username'),
+            'password' => config('returbutiken.settings.users.administrator.password')
+        ]);
+
+        $admin->assignRole('admin');
     }
 }
