3f89c195d60917104e3dc2c8e3ee48bd631cbd05
ToxicTree <zoi009900@gmail.com>
2016-10-03 14:44:42 +0200

    index.blade, gitignore builds

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="/js/app.js?date={{ date('ymdHis') }}"></script>
+    </body>
+</html>
