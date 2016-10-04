111573378b0455f94b857283f6c411747e984afa
ToxicTree <zoi009900@gmail.com>
2016-10-04 15:42:35 +0200

    Loading: New global component ready

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -18,13 +18,28 @@
 </template>
 
 <script>
+    /**
+     * Define global components
+     */
+    Vue.component('loading', require('./components/Loading.vue'));
+
+    /**
+     * Export the root-instance options
+     */
     export default {
 
         name: 'App',
+
+        data: function(){
+            return {
+                loading: true,
+            }
+        },
 
         created: function(){
             // Log a reference to this App
             console.log(this)
         }
+
     }
 </script>
--- resources/assets/js/pages/Articles.vue
+++ resources/assets/js/pages/Articles.vue
@@ -3,35 +3,43 @@
 
         <h2>Articles</h2>
 
-        <div v-if="ithems.length > 0">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Desc</td>
-                        <td>Public</td>
-                        <td>Sale</td>
-                        <td>Bidding</td>
-                        <td>Updated</td>
-                        <td>Tools</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="ithem in ithems">
-                        <td>{{ithem.name}}</td>
-                        <td>{{ithem.desc}}</td>
-                        <td>{{ithem.public}}</td>
-                        <td>{{ithem.publish_interval}}</td>
-                        <td>{{ithem.bidding_interval}}</td>
-                        <td>{{ithem.updated_at}}</td>
-                        <td>X E</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div v-if="this.$root.loading">
+            <loading></loading>
         </div>
 
         <div v-else>
-            Empty
+
+            <div v-if="ithems.length > 0">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Desc</td>
+                            <td>Public</td>
+                            <td>Sale</td>
+                            <td>Bidding</td>
+                            <td>Updated</td>
+                            <td>Tools</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="ithem in ithems">
+                            <td>{{ithem.name}}</td>
+                            <td>{{ithem.desc}}</td>
+                            <td>{{ithem.public}}</td>
+                            <td>{{ithem.publish_interval}}</td>
+                            <td>{{ithem.bidding_interval}}</td>
+                            <td>{{ithem.updated_at}}</td>
+                            <td>X E</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div v-else>
+                Empty
+            </div>
+
         </div>
 
     </div>
@@ -52,9 +60,17 @@
 
             getArticles(){
 
+                this.$root.loading = true;
+
                 this.$http.get('api/articles').then(
-                    (response) => this.ithems = response.data,
-                    (response) => console.error(response.error)
+                    (response) => {
+                        this.ithems = response.data;
+                        this.$root.loading = false;
+                    },
+                    (response) => {
+                        console.error(response.error);
+                        this.$root.loading = false;
+                    }
                 );
             }
 
