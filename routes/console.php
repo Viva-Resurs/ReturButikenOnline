b3bb0214b46eef5c2d9eb98f4f42bc1b3f44bd19
ToxicTree <zoi009900@gmail.com>
2016-10-06 13:19:44 +0200

    Console: updates to article

--- routes/console.php
+++ routes/console.php
@@ -17,10 +17,12 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
 
-Artisan::command('generate:articles', function () {
-    factory(App\Article::class, 10)->create();
+Artisan::command('articles:generate {nr=1}', function ($nr) {
+    factory(App\Article::class, (int) $nr)->create();
+    echo "Generated $nr articles.";
 })->describe('Generate Articles');
 
-Artisan::command('clear:articles', function () {
+Artisan::command('articles:clear', function () {
     DB::table('articles')->delete();
+    echo "Cleared all articles";
 })->describe('Clear Articles');
