957507e3b870f67f2309621b4aa9e3ebc2db6d62
ToxicTree <zoi009900@gmail.com>
2016-10-06 14:05:17 +0200

    Articles: Create - xhttp & some updates

--- resources/assets/js/pages/articles/create.vue
+++ resources/assets/js/pages/articles/create.vue
@@ -1,98 +1,120 @@
 <template>
-<div id="articleForm">
-    <div class="panel-heading">
-        Skapa artikel
-    </div>
+    <div id="articleForm">
 
-    <form class="form-vertical" v-on:submit.prevent="attemptCreate" role="form" name="myform">
-
-        <div class="panel-body">
-
-            <div class="form-group">
-              <div class="row">
-                <div class="col-xs-8">
-                  <label class="control-label" for="name">Varunamn:</label>
-                  <input type="email" class="form-control" v-model="article.name" id="name" placeholder="Varunamn">
-                </div>
-
-                <div class="col-xs-4">
-                  <label class="control-label" for="category">Välj varukategori:</label>
-                  <div class="dropdown">
-                      <button class="btn btn-default dropdown-toggle" type="button" id="category" data-toggle="dropdown">
-                        Kategori<span class="caret"></span></button>
-                      <ul class="dropdown-menu" role="menu" aria-labelledby="category">
-                          <li role="presentation"><a role="menuitem" href="#">Category item</a></li>
-                      </ul>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-
-            <div class="form-group">
-              <div class="row">
-                <div class="col-xs-12">
-                <label class="control-label" for="desc">Beskrivning av varan:</label>
-                <textarea rows="4" class="form-control" v-model="article.desc" id="desc" placeholder="Ge din beskrivning här"></textarea>
-              </div>
-              </div>
-            </div>
-
-            <div class="form-group">
-                <div class="row">
-                  <div class="col-xs-12">
-                  <label class="checkbox-inline">
-                    <input type="checkbox" value="" v-model="article.public"> Publicera inom datumintervall
-                  </label>
-                  <label class="checkbox-inline" >
-                    <input type="checkbox" value="" v-model="article.public"> Aktivera budgivning
-                  </label>
-                </div>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="row">
-                  <div class="col-xs-12">
-                  <label class="radio-inline">
-                    <input type="radio" name="publish" value="" v-model="article.publish_interval"> Publicera på kommunens Intranät
-                  </label>
-                  <label class="radio-inline">
-                    <input type="radio" name="bidding" value="" v-model="article.bidding_interval"> Publicera för allmänheten
-                  </label>
-                </div>
-                </div>
-           </div>
-
-           <div class="form-group">
-             <div class="row">
-               <div class="col-xs-6">
-                 <label class="control-label" for="fullname">Namn kontaktperson:</label>
-                 <input type="text" class="form-control" id="fullname" placeholder="Ditt namn">
-               </div>
-               <div class="col-xs-6">
-                 <label class="control-label" for="phone">Telefon kontaktperson:</label>
-                 <input type="text" class="form-control" id="phone" placeholder="Ditt nummer">
-               </div>
-             </div>
-             <div class="row">
-               <div class="col-xs-12">
-                 <label class="control-label" for="email">E-post kontaktperson:</label>
-                 <input type="email" class="form-control" id="email" placeholder="Din epost">
-               </div>
-             </div>
-           </div>
-
-         </div>
-        <div class="panel-footer text-right">
-            <button type="submit" class="btn btn-default">Förhandsgranska</button>
-            <button type="submit" class="btn btn-primary" @keydown.enter.prevent="attemptCreate">
-        Publicera
-      </button>
+        <div class="panel-heading">
+            Skapa artikel
         </div>
 
-    </form>
-</div>
+        <form class="form-vertical" v-on:submit.prevent="attemptCreate" role="form" name="myform">
+
+            <div class="panel-body">
+
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-xs-10">
+                            <label class="control-label" for="name">Varunamn:</label>
+                            <input class="form-control"
+                                id="name"
+                                type="text"
+                                v-model="article.name"
+                                placeholder="Varunamn"
+                            >
+                        </div>
+                        <div class="col-xs-2" style="white-space: nowrap;">
+                            <div class="pull-right">
+                                <label class="control-label" for="category">Välj varukategori:</label>
+                                <div class="dropdown text-right">
+                                    <button class="btn btn-default dropdown-toggle"
+                                        id="category"
+                                        type="button"
+                                        data-toggle="dropdown"
+                                    >
+                                        Kategori <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu" role="menu" aria-labelledby="category">
+                                        <li role="presentation">
+                                            <a role="menuitem" href="#">Category item</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <label class="control-label" for="desc">Beskrivning av varan:</label>
+                            <textarea class="form-control" rows="4"
+                                id="desc"
+                                v-model="article.desc"
+                                placeholder="Ge din beskrivning här"
+                            ></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <label class="checkbox-inline">
+                                <input type="checkbox" value="" v-model="article.publish_interval">
+                                Publicera inom datumintervall
+                            </label>
+                            <label class="checkbox-inline">
+                                <input type="checkbox" value="" v-model="article.bidding_interval">
+                                Aktivera budgivning
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <label class="radio-inline">
+                                <input type="radio" name="public" v-model="article.public" value="on">
+                                Publicera på kommunens Intranät
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="public" v-model="article.public" value="off"> Publicera för allmänheten
+                            </label>
+                        </div>
+                    </div>
+               </div>
+
+               <div class="form-group">
+                   <div class="row">
+                       <div class="col-xs-6">
+                           <label class="control-label" for="fullname">Namn kontaktperson:</label>
+                           <input type="text" class="form-control" id="fullname" placeholder="Ditt namn">
+                       </div>
+                       <div class="col-xs-6">
+                           <label class="control-label" for="phone">Telefon kontaktperson:</label>
+                           <input type="text" class="form-control" id="phone" placeholder="Ditt nummer">
+                       </div>
+                    </div>
+                    <div class="row">
+                       <div class="col-xs-12">
+                           <label class="control-label" for="email">E-post kontaktperson:</label>
+                           <input type="email" class="form-control" id="email" placeholder="Din epost">
+                       </div>
+                   </div>
+               </div>
+
+            </div>
+
+            <div class="panel-footer text-right">
+                <button type="submit" class="btn btn-default">Förhandsgranska</button>
+                <button type="submit" class="btn btn-primary" @keydown.enter.prevent="attemptCreate">
+                    Publicera
+                </button>
+            </div>
+
+        </form>
+
+    </div>
 </template>
 
 <style>
@@ -113,10 +135,19 @@ export default {
     },
     methods: {
         attemptCreate() {
-
+            this.createArticle();
         },
         createArticle() {
-            var self = this;
+
+            this.$http.post('api/articles',this.article).then(
+                (response) => {
+                    console.log('ok');
+                    this.$router.push({ path: '/articles' });
+                },
+                (response) => {
+                    console.error(response.error);
+                }
+            );
 
         }
 
