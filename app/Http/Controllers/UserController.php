6a62365733a99356f8ad9641798e45e5b0b3acdf
ToxicTree <zoi009900@gmail.com>
2016-10-11 15:32:17 +0200

    UserController: testing Auth:user() at index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use App\User;
 
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
 	public function index(){
+
+		$user = Auth::user();
+
+		if (!$user)
+			abort(401,'Not allowed to list users');
+
         return User::all();
     }
 
