265f69895159ed85eb3aa8bd817335623ea265cf
ToxicTree <zoi009900@gmail.com>
2016-10-17 11:15:13 +0200

    SuccessHandler: handle success emits

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -24,13 +24,14 @@
     import Navigation from './components/Nav.vue'
     import Auth from './mixins/Auth.vue'
     import ErrorHandler from './mixins/ErrorHandler.vue'
+    import SuccessHandler from './mixins/SuccessHandler.vue'
     export default {
 
         name: 'App',
 
         components: { Navigation },
 
-        mixins: [ Auth, ErrorHandler ],
+        mixins: [ Auth, ErrorHandler, SuccessHandler ],
 
         data: function(){
             return {
---
+++ resources/assets/js/mixins/SuccessHandler.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+
+        name: 'ErrorHandler',
+
+        methods: {
+
+            handleSuccess(message) {
+
+                console.log(message);
+
+            }
+
+        },
+
+        mounted: function() {
+
+            // Listen for success
+            bus.$on('success', this.handleSuccess );
+
+        }
+
+    }
+</script>
