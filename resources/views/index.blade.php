29667900aeb72507d88b2a9e27098e85fa1ff262
ToxicTree <zoi009900@gmail.com>
2016-11-01 13:19:41 +0100

    Added Semantic UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,13 @@
 <html>
     <head>
         <title>{{ config('app.name', 'Index') }}</title>
-        
+
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="dist/semantic.min.css">
+        <!-- <link rel="stylesheet" href="/css/app.css"> -->
 
         <!-- Scripts -->
         <script>
@@ -20,5 +21,6 @@
     <body>
         <div id="app"></div>
         <script src="/js/app.js?date={{ date('ymdHis') }}"></script>
+        <script src="dist/semantic.min.js"></script>
     </body>
 </html>
