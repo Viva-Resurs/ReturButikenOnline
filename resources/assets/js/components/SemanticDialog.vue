29afb7447e4e6c4d25aacf2370d08ed2cb76d76a
Daniel Ögren <danielogren82@gmail.com>
2016-12-09 13:22:44 +0100

    Components/SemanticDialog: Added Semantic UI modal based component for displaying messages

---
+++ resources/assets/js/components/SemanticDialog.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+    div.ui.modal.event-modal
+        div.content
+            div.ui.grid.center.aligned
+                div.row
+                    i(":class"="[icon]")
+                div.row
+                    h2 {{ title }}
+                div.row
+                    p {{ message }}
+
+        div.actions
+            div.ui.deny.black.button Close
+</template>
+
+<script lang="coffee">
+    module.exports = {
+        name: 'SemanticDialog'
+        data: ->
+            icon: 'i'
+            title: 'Empty'
+            message: 'Empty'
+
+        methods:
+            handleMessage: (message) ->
+                switch message.type
+                    when "error" then @icon = 'massive circle warning icon'
+                    else
+                        @icon = 'massive info cirle icon'
+
+                @title = message.title
+                @message = message.message
+                $('.modal').modal('show');
+
+        mounted: ->
+            bus.$on('show_message', (message) => @handleMessage(message) );
+    }
+</script>
