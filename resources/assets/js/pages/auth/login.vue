2ae1bff7890013585f5ba027b02f37e69f54eba4
ToxicTree <zoi009900@gmail.com>
2016-12-13 13:40:59 +0100

    Login: converted to coffee

--- resources/assets/js/pages/auth/login.vue
+++ resources/assets/js/pages/auth/login.vue
@@ -23,43 +23,32 @@
             br
 </template>
 
-<style>
-</style>
-
-<script>
-export default {
-    name: 'Login',
-    data: function() {
-        return {
-            login: {
+<script lang="coffee">
+    module.exports = {
+        name: 'Login'
+        data: () ->
+            login:
                 name: '',
                 email: '',
                 password: ''
-            },
             myform: []
-        }
-    },
-    methods: {
-        attemptLogin(tries) {
-            this.$http.post('login',this.login).then(
-                (response) => bus.$emit('login_ok'),
-                (response) => {
-                    // If login fails on token, try one more time
-                    if (tries!=2 && response.data.error == 'TokenMismatch'){
-                        this.$http.get('token').then(
-                            (response) => {
-                                // New token ready, try to login again
-                                sessionStorage.token = response.data.token;
-                                this.attemptLogin(2);
-                            },
-                            (response) => bus.$emit('error',response)
-                        );
-                    }
-                    else
-                        bus.$emit('error',response.data)
-                }
-            );
-        }
+        methods:
+            attemptLogin: (tries) ->
+                @$http.post('login',@login).then(
+                    (response) =>
+                        bus.$emit('login_ok')
+                    (response) =>
+                        # If login fails on token, try one more time
+                        if (tries!=2 && response.data.error == 'TokenMismatch')
+                            @$http.get('token').then(
+                                (response) =>
+                                    # New token ready, try to login again
+                                    sessionStorage.token = response.data.token;
+                                    @attemptLogin(2);
+                                (response) => bus.$emit('error',response)
+                            );
+                        else
+                            bus.$emit('error',response.data)
+                );
     }
-}
 </script>
