83f810426de90e615c66085f613806309a3d6bb6
ToxicTree <zoi009900@gmail.com>
2016-12-13 13:42:53 +0100

    SuccessHandler: converted to coffee, use emit instead of log

--- resources/assets/js/mixins/SuccessHandler.vue
+++ resources/assets/js/mixins/SuccessHandler.vue
@@ -1,24 +1,15 @@
-<script>
-    export default {
-
-        name: 'SuccessHandler',
-
-        methods: {
-
-            handleSuccess(message) {
-
-                console.log(message);
-
-            }
-
-        },
-
-        mounted: function() {
-
-            // Listen for success
+<script lang="coffee">
+    module.exports = {
+        name: 'SuccessHandler'
+        methods:
+            handleSuccess: (message) ->
+                bus.$emit('show_message',{
+                    type: 'success',
+                    title: message.title,
+                    message: message.details
+                });
+        mounted: () ->
+            # Listen for success
             bus.$on('success', this.handleSuccess );
-
-        }
-
-    };
+    }
 </script>
