c485729214f8657ca9ad399acff066570d0a9ff8
Daniel Ögren <danielogren82@gmail.com>
2016-12-16 12:03:31 +0100

    components/ArticlePreview: Changed some of the styling and added more interactive preview of images

--- resources/assets/js/components/ArticlePreview.vue
+++ resources/assets/js/components/ArticlePreview.vue
@@ -5,60 +5,67 @@
 
         div.ui.fluid.raised.card
             div.content
-                div.ui.segment
-                    div.top.attached.ui.secondary.label
-                    | Selected images
+                div.ui.top.attached.segment
+                    div.top.attached.ui.secondary.label Article
+
+                    div.center.aligned
+                        img.ui.fluid.image(v-if="selected_image" ":src"="selected_image.path")
+
+                    br
 
                     div.ui.tiny.images
-                        div( v-for="image in article.selected_images" )
-                            img.ui.tiny.rounded.image( ":src"="image.thumb_path" )
+                        img.ui.image(
+                            ":src"="image.thumb_path"
+                            v-for="image in article.selected_images"
+                            @click="setSelectedImage(image)"
+                            ":class" = "(image.path == selected_image.path) ? 'bordered disabled' : '' ")
 
-                div.header
-                | {{ article.name }}
 
-                div( v-for="category in article.selected_categories" )
-                    div.ui.black.horizontal.label( v-for="category in article.selected_categories")
-                    | {{ category.name }}
+                div.ui.bottom.attached.segment
+                    div.ui.header  {{ article.name }}
 
-                br
-                br
+                    //TODO : Needs original category list to identify category from id
+                    div(v-for="orig_category in categories")
+                        div( v-for="selected_category in article.selected_categories")
+                            div.ui.black.horizontal.label(v-if="(orig_category.id == selected_category)") {{ orig_category.name }}
 
-                div.description
-                    | {{ article.desc }}
+                    div.description {{ article.desc }}
+
+                div.ui.top.attached.segment
+                    div.top.attached.ui.secondary.label Additional information
+                    div.ui.grid.two.columns
+                        div.column
+                            strong Publish interval
+                            p {{ article.publish_interval }}
+
+                        div.column
+                            strong Bidding interval
+                            p {{ article.bidding_interval }}
+
+                    br
+                    div(v-if="article.public")
+                        | Publicerad externt
+                    div(v-else)
+                        | Publicerad på kommunens intranät
+
+                div.ui.bottom.attached.segment
+                    div( v-if="article.selected_contacts")
+                        p
+                            strong Contacts
+
+                        div.ui.card( v-for="contact in article.selected_contacts" )
+                            div.content
+                                div.header
+                                | {{ contact.fullname }}
+
+                                div.description
+                                    p Phone
+                                    b {{ contact.phone }}
+
+                                    p Email
+                                    b {{ contact.email }}
 
             div.extra.content
-                div.ui.grid.two.columns
-                    div.column
-                        strong Publish interval
-                        p {{ article.publish_interval }}
-
-                    div.column
-                        strong Bidding interval
-                        p {{ article.bidding_interval }}
-
-                br
-                div(v-if="article.public")
-                    | Publicerad externt
-                div(v-else)
-                    | Publicerad på kommunens intranät
-
-            div.extra.content
-                div( v-if="article.selected_contacts")
-                    p
-                        strong Contacts
-
-                    div.ui.card( v-for="contact in article.selected_contacts" )
-                        div.content
-                            div.header
-                            | {{ contact.fullname }}
-
-                            div.description
-                                p Phone
-                                b {{ contact.phone }}
-
-                                p Email
-                                b {{ contact.email }}
-
                 div.ui.right.floated.button.primary(
                     @click="attemptPublish"
                 ) Publish
@@ -76,10 +83,18 @@
 
         props: [ 'article' ],
 
+        data: ->
+            selected_image: false;
+
         methods:
             attemptPublish: ->
                 bus.$emit( 'article_form_update', this.article );
 
             modifyArticle: ->
                 bus.$emit( 'article_form_modify' );
+
+            setSelectedImage: ( image ) ->
+                @selected_image = image
+                console.log @selected_image.path
+
 </script>
