3a45a6bbfaf6f1ff07ca082c39b3a74da1676e2a
Daniel Ögren <danielogren82@gmail.com>
2016-12-20 11:46:34 +0100

    Components/SemanticDialog: Some changes to the appearance of the dialog. Callbacks are now supported on confirmation.

--- resources/assets/js/components/SemanticDialog.vue
+++ resources/assets/js/components/SemanticDialog.vue
@@ -1,39 +1,80 @@
 <template lang="pug">
-    div.ui.modal.event-modal
+    div.ui.basic.small.modal.event-modal
         div.content
-            div.ui.grid.center.aligned
-                div.row
-                    i(":class"="[icon]")
-                div.row
-                    h2 {{ title }}
-                div.row
+            div(":class"="[selected_action.class]")
+                i(":class"="[selected_action.icon]")
+                div.class.content
+                    div.header {{ title }}
                     p {{ message }}
-
-        div.actions
-            div.ui.deny.black.button Close
+            div.ui.grid.component.padded( style="position: relative; top: -11px;" )
+                div.center.aligned.column.actions( style="background: rgba(0,0,0,0.1);")
+                    div(v-for="button in selected_action.buttons"
+                    ":class"="[button.class]") {{button.label}}
 </template>
 
 <script lang="coffee">
     module.exports = {
         name: 'SemanticDialog'
         data: ->
-            icon: 'i'
+            selected_action: false
+
+            actions:
+                default:
+                    class: 'ui info icon message'
+                    icon: 'huge center aligned circular info icon'
+                    buttons: [
+                        {
+                            class: 'ui deny inverted blue button',
+                            label: 'Close'
+                        },
+                    ]
+
+                error:
+                    class: 'ui warning icon message'
+                    icon: 'huge center aligned circular red warning icon'
+                    buttons: [
+                        {
+                            class: 'ui deny inverted red button',
+                            label: 'Close'
+                        },
+                    ]
+
+                confirm:
+                    class: 'ui positive icon message'
+                    icon: 'huge center aligned circular blue question icon'
+                    buttons: [
+                        {
+                            class: 'ui deny inverted red button',
+                            label: 'Abort'
+                        },
+                        {
+                            class: 'ui approve inverted green button',
+                            label: 'Confirm'
+                        },
+                    ]
             title: 'Empty'
             message: 'Empty'
 
         methods:
-            # TODO: Confirm method needed
-            confirm: (message,cb) ->
-                # do something with message
-                # yes = run cb(), no = just close
             handleMessage: (message) ->
-                switch message.type
-                    when "error" then @icon = 'massive circle warning icon'
-                    else
-                        @icon = 'massive info cirle icon'
-
                 @title = message.title
                 @message = message.message
+                console.log message
+
+                switch message.type
+                    when "error"
+                        @selected_action = @actions['error']
+                    when "confirm"
+                        @selected_action = @actions['confirm']
+                        $('.modal').modal({
+                            closable: false
+                            onApprove: ->
+                                return message.cb();
+
+                        })
+                    else
+                        @selected_action = @actions['default']
+
                 $('.modal').modal('show');
 
         mounted: ->
