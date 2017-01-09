64a899a3fd83ea0848672901f87df3f6233bedc9
ToxicTree <zoi009900@gmail.com>
2017-01-09 11:57:10 +0100

    Always get fresh css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>{{ config('app.name', 'Index') }}</title>
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/app.css?date={{ date('ymdHis') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
