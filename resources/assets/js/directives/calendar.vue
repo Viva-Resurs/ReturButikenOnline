abf60f291fa594e1dcfeaaed7f4c69e0b3ad7558
ToxicTree <zoi009900@gmail.com>
2017-01-10 13:14:41 +0100

    Calendar: Formatter settings

--- resources/assets/js/directives/calendar.vue
+++ resources/assets/js/directives/calendar.vue
@@ -7,9 +7,19 @@
                     ampm: false
                     inline: true
                     endCalendar: $('#interval_end')
+                    formatter:
+                        date: (date) ->
+                            return moment(date).format('YYYY-MM-DD')
+                        time: (date, settings, forCalendar) ->
+                            return moment(date).format('HH:mm')
             if el.id == "interval_end"
                 $(el).calendar
                     ampm: false
                     inline: true
                     startCalendar: $('#interval_start')
+                    formatter:
+                        date: (date) ->
+                            return moment(date).format('YYYY-MM-DD')
+                        time: (date, settings, forCalendar) ->
+                            return moment(date).format('HH:mm')
 </script>
