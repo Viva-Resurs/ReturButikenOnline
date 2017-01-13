bc92150aef712f5e6ea0043447475441e847fff0
Daniel Ögren <danielogren82@gmail.com>
2017-01-13 10:27:54 +0100

    tools/Preview: Added a new preview tool

---
+++ resources/assets/js/components/tools/Preview.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    div.ui.icon.button.hover-primary(
+        v-tooltip = ""
+        data-html = "Preview"
+        @click    = "preview(item)"
+    )
+        i.ui.icon.unhide
+</template>
+
+<script lang="coffee">
+    module.exports =
+        name: 'Preview'
+        props: [ 'item', 'from' ]
+        methods:
+            preview: (item) ->
+                bus.$emit @from + '_item_preview', item
+</script>
