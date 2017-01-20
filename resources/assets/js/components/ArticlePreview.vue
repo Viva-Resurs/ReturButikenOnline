3d9b57072cfde8c5cbca3c98d6e95adf02176fa5
ToxicTree <zoi009900@gmail.com>
2017-01-20 15:08:31 +0100

    ArticlePreview: Updated layout, show/hide details

--- resources/assets/js/components/ArticlePreview.vue
+++ resources/assets/js/components/ArticlePreview.vue
@@ -3,100 +3,93 @@
 
         div.ui.dividing.header( v-if="mode!='show'" ) Förhandsgranskning
 
-        div( v-if="article!=-1" ":class"="(mode!='show')?'ui card fluid raised':''" )
-            div.content
-                div.ui.basic.segment.center.aligned.preview_header( v-swipe="article.selected_images" )
+        div( v-if="article!=-1" )
+            div.ui.basic.segment.center.aligned.preview_header( v-swipe="article.selected_images" )
 
-                div.ui.basic.segment.center.aligned.preview_thumbs
-                    div.ui.buttons
-                        //button.ui.button.icon.black
-                            i.icon.angle.left
-                        div.ui.tiny.left.aligned.images
-                            img.ui.image(
-                                ":src"="'/'+image.thumb_path"
-                                v-for="(image, index) in article.selected_images"
-                                @click="setActiveImage(index)"
-                                ":class" = "(image.selected) ? 'active' : 'disabled' ")
-                        //button.ui.button.icon.black.right.floated
-                            i.icon.angle.right
+            div.ui.basic.segment.center.aligned.preview_thumbs
+                div.ui.buttons
+                    div.ui.tiny.left.aligned.images
+                        img.ui.image(
+                            ":src"="'/'+image.thumb_path"
+                            v-for="(image, index) in article.selected_images"
+                            @click="setActiveImage(index)"
+                            ":class" = "(image.selected) ? 'active' : 'disabled' ")
 
+            div.ui.basic.segment
+                h2.ui.header {{ article.name }}
+                    div.sub.header Artikelnr: {{article.id}}
+                div.description {{ article.desc }}
 
-                div.ui.basic.segment
-                    h2.ui.header {{ article.name }}
-                        div.sub.header Artikelnr: {{article.id}}
-                    div.description {{ article.desc }}
+                br
+                br
+                div.ui.grid.bottom.aligned.stackable
+                    div.two.column.row
+                        div.left.aligned.left.floated.column(v-if="article.selected_contacts")
+                            h4.ui.sub.header Kontakt
+                            div(v-for="contact in contacts")
+                                div( v-for="selected_contact in article.selected_contacts")
+                                    div.ui.card(v-if="(contact.id == selected_contact)")
+                                        div.content
+                                            div.header {{ contact.fullname }}
+                                            div.description
+                                                p
+                                                    i.icon.phone
+                                                    b &nbsp;{{ contact.phone }}
+                                                p
+                                                    i.icon.mail
+                                                    b {{ contact.email }}
+                        div.right.aligned.right.floated.column
+                            h3 Pris : {{article.price}} kr
+                div.ui.hidden.divider
+                div.ui.segment( v-if="mode!='show'" )
+                    div.ui.top.attached.label( @click="toggleDetails()" )
+                        h4.ui.sub.header Mer information
+                    div.ui.grid.equal.width.stackable#details
+                        div.row( v-if="article.selected_categories.length>0" )
+                            div.column
+                                h4.ui.sub.header Kategorier
+                                p
+                                    template(v-for="(orig_category, index) in categories")
+                                        template( v-for="selected_category in article.selected_categories")
+                                            span(v-if="(orig_category.id == selected_category)")
+                                                i.ui.icon.tag( v-if="index==0" )
+                                                span( v-if="index!=0 && article.selected_categories.length>1" )
+                                                    | {{ ', ' + orig_category.name }}
+                                                span( v-else="" )
+                                                    | {{ orig_category.name }}
+                        div.row
+                            div.column( v-if="article.publish_interval" )
+                                h4.ui.sub.header Publicerings intervall
+                                i.ui.icon.time
+                                span  {{ formatInterval(article.publish_interval) }}
 
-                    div.ui.basic.segment.right.aligned.right.floated
-                        h3 Pris : {{article.price}} kr
-                    br
-                    br
-                    br
+                            div.column( v-if="article.bidding_interval" )
+                                h4.ui.sub.header Budgivningsintervall
+                                i.ui.icon.time
+                                span  {{ formatInterval(article.bidding_interval) }}
+                        div.row
+                            div.column(v-if="article.public")
+                                h4.ui.sub.header Område
+                                i.ui.icon.green.world
+                                | Publicerad externt
+                            div.column(v-if="!article.public")
+                                h4.ui.sub.header Område
+                                i.ui.icon.red.industry
+                                | Publicerad på kommunens intranät
 
-                div.ui.top.attached.segment
-                    div.top.attached.ui.secondary.label Övrig information
-
-                    div.ui.basic.segment
-                        h4.ui.sub.header Kategorier
-                        p
-                            template(v-for="(orig_category, index) in categories")
-                                template( v-for="selected_category in article.selected_categories")
-                                    span(v-if="(orig_category.id == selected_category)")
-                                        i.ui.icon.tag( v-if="index==0" )
-                                        span( v-if="index!=0 && article.selected_categories.length>1" )
-                                            | {{ ', ' + orig_category.name }}
-                                        span( v-else="" )
-                                            | {{ orig_category.name }}
-
-                    div.ui.basic.segment( v-if="article.publish_interval && mode!='show'" )
-                        h4.ui.sub.header Publicerings intervall
-                        i.ui.icon.time
-                        span  {{ article.publish_interval }}
-
-                    div.ui.basic.segment( v-if="article.bidding_interval && mode!='show'" )
-                        h4.ui.sub.header Budgivningsintervall
-                        i.ui.icon.time
-                        span  {{ article.bidding_interval }}
-
-                    div.ui.basic.segment(v-if="article.public && mode!='show'")
-                        h4.ui.sub.header Område
-                        i.ui.icon.green.world
-                        | Publicerad externt
-                    div.ui.basic.segment(v-if="!article.public && mode!='show'")
-                        h4.ui.sub.header Område
-                        i.ui.icon.red.industry
-                        | Publicerad på kommunens intranät
-
-                    div.ui.basic.segment(v-if="article.selected_contacts")
-                        h4.ui.sub.header Kontakt
-                        div(v-for="orig_contact in contacts")
-                            div( v-for="selected_contact in article.selected_contacts")
-                                div.ui.card(v-if="(orig_contact.id == selected_contact)")
-                                    div.content
-                                        div.header {{ orig_contact.fullname }}
-
-                                        div.description
-                                            p Phone
-                                            b {{ orig_contact.phone }}
-
-                                            p Email
-                                            b {{ orig_contact.email }}
-
-
-            div.extra.content( v-if="mode!='show'" )
-                div.ui.right.floated.button.primary(
+        div.ui.container
+            div.ui.hidden.divider
+            div.ui.container.right.aligned
+                div.ui.button.secondary( v-if="mode=='show'"
+                    @click="goBack()"
+                ) Back
+                div.ui.button.secondary( v-if="mode!='show'"
+                    @click="modifyArticle"
+                ) Modify
+                div.ui.button.primary( v-if="mode!='show'"
                     @click="attemptPublish"
                 ) Publish
 
-                div.ui.right.floated.button.red(
-                    @click="modifyArticle"
-                ) Modify
-
-        div.ui.container( v-if="mode=='show'" )
-            div.ui.hidden.divider
-            div.ui.container.right.aligned
-                div.ui.button.secondary(
-                    @click="goBack()"
-                ) Back
 
         div( v-if="article==-1" )
             | Artikeln hittades inte
@@ -106,8 +99,21 @@
     module.exports =
         name: 'Preview'
         props: [ 'mode', 'article', 'categories', 'contacts' ]
-
+        data: ->
+            expandDetails: @mode
         methods:
+            toggleDetails: ->
+                $('#details').transition
+                    animation  : 'slide down'
+                    onShow : =>
+                        @expandDetails = true
+                    onHide : =>
+                        @expandDetails = false
+
+            formatInterval: (interval) ->
+                dates = String(interval).split '|'
+                return dates[0] + ' - ' + dates[1]
+
             attemptPublish: ->
                 bus.$emit( 'article_form_update', this.article )
 
