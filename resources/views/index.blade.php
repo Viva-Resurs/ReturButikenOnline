a0521c6b5f0dae53bf3d20dcc6745a49156a38e0
ToxicTree <zoi009900@gmail.com>
2017-01-26 13:33:56 +0100

    Index.blade: Use relative path to js/css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <title>{{ config('app.name', 'Index') }}</title>
-        <link rel="stylesheet" href="/css/semantic.min.css">
-        <link rel="stylesheet" href="/css/app.css?date={{ date('ymdHis') }}">
+        <link rel="stylesheet" href="css/semantic.min.css">
+        <link rel="stylesheet" href="css/app.css?date={{ date('ymdHis') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <div id="app"></div>
-        <script src="/js/app.js?date={{ date('ymdHis') }}"></script>
+        <script src="js/app.js?date={{ date('ymdHis') }}"></script>
     </body>
 </html>
