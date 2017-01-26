2cdb32ecbe37d3ee1ae87de20f0ccd0bc0ea7088
ToxicTree <zoi009900@gmail.com>
2017-01-26 15:13:06 +0100

    Index.blade: Set base href by using $_SERVER and then keep all path relative. (this will make App connections work when runned in a subdir)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>{{ config('app.name', 'Index') }}</title>
+        <base href="{{ substr($_SERVER['SCRIPT_NAME'],0,strripos($_SERVER['SCRIPT_NAME'],'/')+1) }}">
         <link rel="stylesheet" href="css/semantic.min.css">
         <link rel="stylesheet" href="css/app.css?date={{ date('ymdHis') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
