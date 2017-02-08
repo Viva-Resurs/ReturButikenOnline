69eaba948a5ec74876b8ddb2f600765edd008fce
ToxicTree <zoi009900@gmail.com>
2017-02-08 13:33:19 +0100

    Profile: Changed to work as a redirect to update (when user is ready)

--- resources/assets/js/pages/users/profile.vue
+++ resources/assets/js/pages/users/profile.vue
@@ -1,71 +1,20 @@
 <template lang="pug">
-    div
-        div.ui.attached( v-if="$root.loading" )
-            loading
-        user-form(
-            v-if="!$root.loading && user!=null"
-            ":draft"="user"
-            ":roles"="roles"
-            ":sections"="sections" )
+    div.ui.attached
+        loading
 </template>
 
 <script lang="coffee">
     module.exports =
-        name: 'Profile'
-        components:
-            UserForm: require '../../components/UserForm.vue'
-        data: ->
-            user: null
-            roles: false
-            sections: false
+        name: 'Profile Redirect'
         methods:
-            getMyUser: (user) ->
-                @$root.loading = true
-                @$http.get('api/users/'+user.id).then(
-                    (response) =>
-                        @user = response.data
-                        @$root.loading = false
-                    (response) =>
-                        bus.$emit 'error', response.data
-                        @$root.loading = false
-                )
-
-            getRoleList: ->
-                @$http.get('api/roles').then(
-                    (response) =>
-                        @roles = response.data
-                    (response) =>
-                        bus.$emit 'error', response
-                        @roles = []
-                )
-
-            getSectionList: ->
-                @$http.get('api/sections').then(
-                    (response) =>
-                        @sections = response.data
-                    (response) =>
-                        bus.$emit 'error', response
-                        @sections = []
-                )
-
-            updateUser: (user) ->
-                @$http.put('api/users/'+user.id,user).then(
-                    (response) =>
-                        @$router.push path: '/users'
-                    (response) => bus.$emit 'error', response.data
-                )
-        mounted: ->
+            goToUpdate: ->
+                @$router.push path: '/users/'+@$root.user.id
+        created: ->
+            # Check if User is ready or wait
             if @$root.user
-                @getMyUser @$root.user
+                @goToUpdate()
             else
-                bus.$on 'user_changed', @getMyUser
-            # Listen for changes
-            bus.$on 'user_form_update', (payload) => @updateUser payload
-            # Get roles
-            @getRoleList()
-            # Get sections
-            @getSectionList()
+                bus.$on 'user_changed', @goToUpdate
         beforeDestroy: ->
-            bus.$off 'user_form_update'
-            bus.$off 'user_changed', @getMyUser
+            bus.$off 'user_changed', @goToUpdate
 </script>
