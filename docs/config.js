2dd2e7d6055c2796e90dbaadcf8c61da50f23652
ToxicTree <zoi009900@gmail.com>
2017-03-01 15:21:56 +0100

    Docs: Added my batch-showdown module to generate docs

---
+++ docs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "css": [
+        "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css"
+    ],
+    "js": [
+        "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"
+    ],
+    "onload": "hljs.initHighlighting();"
+}
