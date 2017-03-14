5d8d11cd04a7e6022c02deac61cc6db3fb5a2438
Daniel Ögren <danielogren82@gmail.com>
2017-03-14 13:56:29 +0100

    article/mobile/list: Initial menu

--- resources/assets/vue/components/article/mobile/list.vue
+++ resources/assets/vue/components/article/mobile/list.vue
@@ -2,69 +2,72 @@
     div
         loading( v-if="$root.loading" )
         div( v-else="" )
-            div.ui.secondary.inverted.menu.top.attached
+            div.ui.top.attached.menu
                 div.left.menu
-                    div.ui.icon.input
-                        input.prompt(
-                            v-model="search"
-                            ":placeholder"="translate('placeholder.search')"
-                            )
-                        i.search.icon
-                div.right.menu
-                    div.ui.floated.basic.button.dropdown#order( v-dropdown="" )
-                        div.default.value
+                    div.ui.search.item
+                        div.ui.transparent.icon.input(style="width: 150px")
+                            input.prompt(
+                                v-model="search"
+                                ":placeholder"="translate('placeholder.search')"
+                                )
+                            i.search.icon
 
-                            i.icon.label.font(
+                div.ui.right.dropdown.item#order( v-dropdown="" )
+                    div.default.value
+
+                        i.icon.label.sort( ":class" = "(desc == 1)?'ascending':'descending'" )
+
+                        i.icon.label.font(
+                            v-if="order=='name'"
+                        )
+                        i.icon.label.wait(
+                            v-if="order=='updated_at'"
+                        )
+                        i.icon.label.pie.chart(
+                            v-if="order=='categories'"
+                        )
+                        i.icon.label.universal.access(
+                            v-if="order=='public'"
+                        )
+
+
+                    div.menu
+                        div.item(
+                            ":class"="(order=='name')?'active':''"
+                            @click="setOrder('name')"
+                            )
+                            | {{ translate('name') }} &nbsp;
+                            i.icon.label.sort(
                                 v-if="order=='name'"
+                                ":class" = "(desc)?'ascending':'descending'"
                             )
-                            i.icon.label.wait(
+                        div.item(
+                            ":class"="(order=='updated_at')?'active':''"
+                            @click="setOrder('updated_at')"
+                            )
+                            | {{ translate('updated') }} &nbsp;
+                            i.icon.label.sort(
                                 v-if="order=='updated_at'"
+                                ":class" = "(desc)?'ascending':'descending'"
                             )
-                            i.icon.label.pie.chart(
+                        div.item(
+                            ":class"="(order=='categories')?'active':''"
+                            @click="setOrder('categories')"
+                            )
+                            | {{ translate('categories') }} &nbsp;
+                            i.icon.label.sort(
                                 v-if="order=='categories'"
+                                ":class" = "(desc)?'ascending':'descending'"
                             )
-                            i.icon.label.universal.access(
-                                v-if="order=='public'"
+                        div.item(
+                            ":class"="(order=='public')?'active':''"
+                            @click="setOrder('public')"
+                                )
+                                | {{ translate('public') }} &nbsp;
+                                i.icon.label.sort(
+                                    v-if="order=='public'"
+                                    ":class" = "(desc)?'ascending':'descending'"
                             )
-
-
-                        div.menu
-                            div.item(
-                                ":class"="(order=='name')?'active':''"
-                                @click="setOrder('name')"
-                                )
-                                | {{ translate('name') }} &nbsp;
-                                i.icon.label.sort(
-                                    v-if="order=='name'"
-                                    ":class" = "(desc)?'ascending':'descending'"
-                                )
-                            div.item(
-                                ":class"="(order=='updated_at')?'active':''"
-                                @click="setOrder('updated_at')"
-                                )
-                                | {{ translate('updated') }} &nbsp;
-                                i.icon.label.sort(
-                                    v-if="order=='updated_at'"
-                                    ":class" = "(desc)?'ascending':'descending'"
-                                )
-                            div.item(
-                                ":class"="(order=='categories')?'active':''"
-                                @click="setOrder('categories')"
-                                )
-                                | {{ translate('categories') }} &nbsp;
-                                i.icon.label.sort(
-                                    v-if="order=='categories'"
-                                    ":class" = "(desc)?'ascending':'descending'"
-                                )
-                            div.item(
-                                ":class"="(order=='public')?'active':''"
-                                @click="setOrder('public')"
-                                    )
-                                    | {{ translate('public') }} &nbsp;
-                                    i.icon.label.sort(
-                                        v-if="order=='public'"
-                                        ":class" = "(desc)?'ascending':'descending'"
-                                )
             div.ui.padded.grid
                 div.row.top.attached
                     article-card(
@@ -101,6 +104,7 @@
             ArticleCard: require '../card.vue'
         data: ->
             order: 'updated_at'
+            desc: 1
         computed:
             from: ->
                 @$route.path.substring 1
