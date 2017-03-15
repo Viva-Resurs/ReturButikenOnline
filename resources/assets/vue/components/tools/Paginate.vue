e940ce4e2e339980515b7e94406753c3a49bbadd
Daniel Ögren <danielogren82@gmail.com>
2017-03-15 13:51:42 +0100

    tools/Paginate: Added initial tool for pagination

---
+++ resources/assets/vue/components/tools/Paginate.vue
@@ -0,0 +1,161 @@
+<template lang="pug">
+    div.ui.grid.equal.width
+        div.column.mobile.only
+            div.ui.button.icon.item.dropdown( v-dropdown="" )
+                i.sticky.note.icon
+                div.ui.menu
+                    div.header Navigate
+                    div.row
+                        select.selection.fluid.dropdown#limit(
+                            v-show="total>limitOptions[0]"
+                            v-dropdown="" v-model="limit" )
+                            option.item( v-for="option in limitOptions" ":value"="option") {{option}}
+
+                    div.five.ui.basic.buttons
+                        button.ui.button( @click="firstPage" )
+                            i.angle.double.left.icon
+                        button.ui.button( @click="prevPage" )
+                            i.angle.left.icon
+                        button( v-for="n in totalPages" ":class"="'ui button '+((n==currentPage)?'active':'')" @click="toPage(n)" ) {{n}}
+
+                        button.ui.button( @click="nextPage" )
+                            i.angle.right.icon
+                        button.ui.button( @click="lastPage" )
+                            i.angle.double.right.icon
+
+        div.row.tablet.only(style="position: relative; left: 15px")
+            div.item( @click="firstPage" )
+                i.angle.double.left.icon
+            div.item( @click="prevPage" )
+                i.angle.left.icon
+            div.item
+                i {{ translate('page') }} {{currentPage}} ... {{totalPages}}
+            div.item( @click="nextPage" )
+                i.angle.right.icon
+            div.item( @click="lastPage" )
+                i.angle.double.right.icon
+
+            select.selection.dropdown.item#limit(
+                v-show="total>limitOptions[0]"
+                v-dropdown="" v-model="limit" )
+                option.item( v-for="option in limitOptions" ":value"="option") {{option}}
+
+        div.column.computer.only
+            div.equal.width.row
+                div.left.floated.column( v-if="total!=0 && total>limit && showPagination && false" )
+                    div.ui.grid.computer.only.pagination.menu
+                        a.icon.item( @click="firstPage" )
+                            i.angle.double.left.icon
+
+                        a.icon.item( @click="prevPage" )
+                            i.angle.left.icon
+
+                        a( v-for="n in totalPages" ":class"="'item '+((n==currentPage)?'active':'')" @click="toPage(n)" ) {{n}}
+
+                        a.icon.item( @click="nextPage" )
+                            i.angle.right.icon
+
+                        a.icon.item( @click="lastPage" )
+                            i.angle.double.right.icon
+
+                    div.ui.grid.mobile.tablet.only.pagination.menu
+                        a.icon.item( @click="firstPage" )
+                            i.angle.double.left.icon
+
+                        a.icon.item( @click="prevPage" )
+                            i.angle.left.icon
+
+                        a.icon.item
+                            i {{ translate('page') }} {{currentPage}} ... {{totalPages}}
+
+                        a.icon.item( @click="nextPage" )
+                            i.angle.right.icon
+
+                        a.icon.item( @click="lastPage" )
+                            i.angle.double.right.icon
+
+                div.right.floated.right.aligned.column
+                    select.selection.fluid.dropdown#limit(
+                        v-show="total>limitOptions[0]"
+                        v-dropdown="" v-model="limit" )
+                        option.item( v-for="option in limitOptions" ":value"="option") {{option}}
+</template>
+
+<script lang="coffee">
+    module.exports =
+        name: 'Paginate'
+        props: ['total', 'showPagination']
+        data: ->
+            offset: 0
+            limit: 10
+            limitOptions: [ 10, 50, 100, 500 ]
+
+        computed:
+
+            currentPage: ->
+                p = 1
+                c = 0
+                i = 0
+                while i < @total
+                    if (i==@offset)
+                        return p;
+                    if (c+1==@limit && i<@total-p)
+                        p++; c=0;
+                    i++
+                    c++
+
+            totalPages: ->
+                p = 1
+                c = 0
+                i = 0
+                while i < @total
+                    if (c+1==@limit && i<@total-p)
+                        p+=1; c=0;
+                    i++
+                    c++
+
+                return p;
+
+        methods:
+            change_search: (value) ->
+                bus.$emit 'search_changed', value
+
+            firstPage: ->
+                @offset = 0;
+
+            prevPage: ->
+                if (@offset-@limit<0)
+                    return @firstPage();
+                @offset -= @limit;
+
+            toPage: (p) ->
+                @offset = (p-1)*@limit;
+
+            nextPage: ->
+                if (@offset>=@total-@limit)
+                    return @lastPage();
+                @offset += @limit;
+
+            lastPage: ->
+                @offset = @total-(((@total-1)%@limit)+1);
+
+        watch:
+            limit: (val, oldVal) ->
+                bus.$emit('limit_changed',val);
+
+            offset: (val, oldVal) ->
+                bus.$emit('offset_changed',val);
+
+        created: ->
+
+            window.onkeyup = (e) =>
+                key = e.which || e.keyCode
+                if (key == 37)
+                    @prevPage()
+                if (key == 39)
+                    @nextPage()
+
+        beforeDestroy: ->
+            window.onkey = false;
+
+</script>
