a443183f50259c95d598a25a94a94a4f8700c703
Daniel Ögren <danielogren82@gmail.com>
2017-03-23 10:34:57 +0100

    mixins/ScreenSize: Added initial mixin for checking the current screen size

---
+++ resources/assets/vue/mixins/ScreenSize.vue
@@ -0,0 +1,32 @@
+<script lang="coffee">
+    module.exports =
+        name: 'ScreenSize'
+        data: ->
+            screensize: 'none'
+            screensizes: 
+                mobile: "(max-width: 767px)"
+                tablet: "(min-width: 768px) and (max-width: 991px)"
+                desktop: "(min-width: 992px)"
+                    
+        methods:
+            WidthChange: (mq, type) ->
+                if (mq.matches)                    
+                    @screensize = type
+                
+        mounted: ->            
+            self = this;
+            mobileMq = window.matchMedia(@screensizes.mobile)            
+            mobileMq.addListener( -> 
+                self.WidthChange(this, "mobile")
+                )
+
+            tabletMq = window.matchMedia(@screensizes.tablet)            
+            tabletMq.addListener( -> 
+                self.WidthChange(this, "tablet")
+                )
+                
+            desktopMq = window.matchMedia(@screensizes.desktop)            
+            desktopMq.addListener( -> 
+                self.WidthChange(this, "desktop")
+                )           
+</script>
