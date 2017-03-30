d2fb993ba9af01efb5a2f88047e84aca13b569c5
Daniel Ögren <danielogren82@gmail.com>
2017-03-30 10:41:13 +0200

    mixins/LangChoice: Changed default language to swedish

--- resources/assets/vue/mixins/LangChoice.vue
+++ resources/assets/vue/mixins/LangChoice.vue
@@ -20,5 +20,5 @@
                 if key == sessionStorage.lang
                     return @setLang key
             # Not available, set default language
-            @setLang 'en'
+            @setLang 'sv'
 </script>
