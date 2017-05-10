22bb220302656a60c9e264b9e0522a1a401418e4
Daniel Ögren <danielogren82@gmail.com>
2017-05-10 08:17:11 +0200

    directives/images: Changed script to use jquery-ui touch punch for handling image related events. It should now work correctly on mobile devices

--- resources/assets/vue/directives/images.vue
+++ resources/assets/vue/directives/images.vue
@@ -4,13 +4,36 @@
             dragged = false # Currently dragged element
             clearStates = ->
                 for element in el.childNodes
-                    element.style.opacity = '1.0'
-            el.onmouseup = clearStates
+                    element.style.opacity = '1.0'            
+            
+            el.addEventListener 'mouseup', clearStates          
+
+            # Set longpress timer to 200ms
+            longPress = ->
+                @longpress_timer = setTimeout =>
+                    @longpress = true
+                    @card.style.opacity = '0.5'
+                , 200
+
+           # If not longpress, treat as normal click  
+            shortPress = (evt) ->
+                if !dragged
+                    if !@longpress
+                        bus.$emit 'show_message',
+                            title: evt.target.card.vmData.original_name
+                            type: 'image'
+                            image: evt.target.card.vmData
+                # Reset
+                clearTimeout @longpress_timer
+                @longpress = false
+    
             setupCards = ->
+                
                 # Setup each card
                 for card, index in el.childNodes
                     # Set references and data-binding
                     card.vmData = binding.value[index] # Reference to the card Vue-data
+
                     for element in card.childNodes
                         if element.tagName == 'IMG'
                             card.image = element # The image of card
@@ -18,22 +41,13 @@
                             # Clear longpress state
                             clearTimeout element.longpress_timer
                             element.longpress = false
-                            # Set longpress timer to 200ms
-                            element.onmousedown = ->
-                                @longpress_timer = setTimeout =>
-                                    @longpress = true
-                                    @card.style.opacity = '0.5'
-                                , 200
-                            element.onmouseup = ->
-                                # If not longpress, treat as normal click
-                                if !@longpress
-                                    bus.$emit 'show_message',
-                                        title: @card.vmData.original_name
-                                        type: 'image'
-                                        image: @card.vmData
-                                # Reset
-                                clearTimeout @longpress_timer
-                                @longpress = false
+                            
+                            element.addEventListener 'mousedown', longPress                            
+                            #element.addEventListener 'touchstart', longPress                            
+                            
+                            element.addEventListener 'mouseup', shortPress, @card
+                            #element.addEventListener 'touchend', shortPress, @card                            
+        
                         if element.tagName == 'A'
                             remover = element # Remove button
                             remover.card = card
@@ -42,49 +56,45 @@
                                 bus.$emit 'image_removed', @card.vmData
                                 # When DOM is ready, setup cards again
                                 Vue.nextTick -> setupCards()
+                    
                     # Set current position
                     card.image.position = index
+
+                    $(card.image).draggable
+                        helper: "clone"
+                        start: (event, ui) ->                        
+                            @card.style.opacity = '0.5'
+                            dragged = this
+                        stop: (event, ui) ->                            
+                            clearStates()
+
                     # When element is dropped upon
-                    card.image.ondrop = (e) ->
-                        e.preventDefault()
-                        target = this
-                        if process.env.NODE_ENV == "development"
-                            console.debug 'Move (d t): ' + dragged.position + ' -> ' + target.position
-                        # Switch positions
-                        temp = dragged.position
-                        dragged.position = target.position
-                        target.position = temp
-                        if process.env.NODE_ENV == "development"
-                            console.debug 'Position (d t): ' + dragged.position + ' ' + target.position
-                        # Clear styles
-                        target.card.style.border = ""
-                        # Update Vue-Data
-                        Vue.set target.card.vmData, 'order', target.position
-                        Vue.set dragged.card.vmData, 'order', dragged.position
-                        # When DOM is ready
-                        Vue.nextTick ->
-                            if process.env.NODE_ENV == "development"
-                                console.debug 'VM (d t): '+ dragged.card.vmData.order + ' ' + target.card.vmData.order
-                            # Clear currently dragged element
-                            dragged = false
-                            # Setup cards again
-                            setupCards()
-                    # When hovering, just return (say it´s ok to drop here)
-                    card.image.ondragover = (e) ->
-                        e.preventDefault()
-                    # When drag enters
-                    card.image.ondragenter = (e) ->
-                        @card.style.border = "3px dotted red"
-                    # When drag leaves
-                    card.image.ondragleave = (e) ->
-                        @card.style.border = ""
-                    # When starting to drag (set currently dragged element)
-                    card.image.ondragstart = (e) ->
-                        @card.style.opacity = '0.5'
-                        dragged = this
-                    # When element is finished draging
-                    card.image.ondragend = (e) ->
-                        clearStates()
+                    $(card.image).droppable
+                        drop: (event, ui) ->  
+                            event.preventDefault()                          
+                            
+                            target = event.target
+                            temp = dragged.position
+                                                        
+                            dragged.position = target.position
+                            
+                            target.position = temp
+                            target.card.style.border = ""
+                            
+                            Vue.set target.card.vmData, 'order', target.position
+                            Vue.set dragged.card.vmData, 'order', dragged.position
+                            Vue.nextTick ->
+                                # Clear currently dragged element
+                                dragged = false
+                                # Setup cards again
+                                setupCards()
+                            
+                            return
+                        out: (event, ui) ->                            
+                            @card.style.border = ""
+                        over: (event, ui) ->           
+                            @card.style.border = "3px dotted red"
+
             # When DOM is ready, setup cards
             Vue.nextTick -> setupCards()
             # Listen for 'refresh' (when adding new images, rebuild cards)
