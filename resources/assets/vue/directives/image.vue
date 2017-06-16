bcf6884876789be40dae9aaf4889accd8f2bc4dc
Daniel Ögren <danielogren82@gmail.com>
2017-06-16 13:13:56 +0200

    directive/image: Image that is displayed move into view in relation to previous navigated image. It is not longer possible to navigate in a wrong direction while swiping a image.

--- resources/assets/vue/directives/image.vue
+++ resources/assets/vue/directives/image.vue
@@ -1,29 +1,29 @@
 <script lang="coffee">
-    module.exports =            
-        componentUpdated: (el, binding) ->           
-            #Global properties
-            
+    module.exports =
+        componentUpdated: (el, binding) ->                       
             imageHeight = ""
             imageWidth = ""
-          
             windowHeight = window.innerHeight-30
             windowWidth = window.innerWidth-50
             position = 0
+            navigated = 0
 
-            leftButtonClicked = (e) ->  
-                bus.$emit 'left_button_clicked'                     
-                               
-            rightButtonClicked = (e) ->
-                bus.$emit 'right_button_clicked'                     
+            leftButtonClicked = (e) ->                                      
+                $('#leftButton, #rightButton, #closeButton').css('visibility', 'hidden')                  
+                $('#leftButton, #rightButton, #closeButton').css('opacity', '0')  
+                bus.$emit 'left_button_clicked'   
+
+            rightButtonClicked = (e) ->                                
+                $('#leftButton, #rightButton, #closeButton').css('visibility', 'hidden')                                                  
+                $('#leftButton, #rightButton, #closeButton').css('opacity', '0')  
+                bus.$emit 'right_button_clicked' 
 
             addOverlayButtons = () ->                
-                # Hide buttons depending on position
-
                 leftButton = document.getElementById "leftButton" 
                 rightButton = document.getElementById "rightButton" 
                 closeButton = document.getElementById "closeButton" 
-                
-                if !leftButton || !rightButton  || !closeButton             
+        
+                if !leftButton || !rightButton  || !closeButton   
                     leftButton = document.createElement 'i'
                     leftButton.className = "huge chevron circle left icon"
                     leftButton.style.zIndex = '1'
@@ -31,13 +31,15 @@
                     leftButton.style.position = 'absolute'   
                     leftButton.style.filter = 'drop-shadow(2px 2px 2px #222)'
                     leftButton.style.left = '20px'
-
+                    
                     rightButton = document.createElement 'i'                
                     rightButton.id = "rightButton"           
                     rightButton.className = "huge chevron circle right icon"                
                     rightButton.style.position = 'absolute'   
                     rightButton.style.filter = 'drop-shadow(2px 2px 2px #222)'             
-                    rightButton.style.zIndex = '1'                
+                    rightButton.style.zIndex = '1'   
+                    rightButton.style.right = 20+'px'
+                  
                     Vue.nextTick ->  
                         leftButton.addEventListener "mouseup", leftButtonClicked
                         rightButton.addEventListener "mouseup", rightButtonClicked      
@@ -59,18 +61,20 @@
                    
                     closeButton.appendChild(closeButtonIcon)                                                      
                     closeButton.onclick = () -> $('.modal').modal('hide')
-
+                    closeButton.style.top = '5px'
+                    closeButton.style.right = '5px'
+          
                     el.appendChild leftButton
                     el.appendChild rightButton
                     el.appendChild closeButton
 
-                closeButton.style.top = '5px'
-                closeButton.style.right = '5px'
                 leftButton.style.top = ((imageHeight-30)/2)+'px'
                 rightButton.style.top = ((imageHeight-30)/2)+'px'
-                rightButton.style.right = 20+'px'
-                rightButton.style.margin = '0px'
-
+                closeButton.style.visibility = "visible"                             
+                $('#leftButton, #rightButton, #closeButton').animate                            
+                    opacity: 1
+                , 1000, null
+            
                 hideButtons(leftButton, rightButton)
 
             hideButtons = (leftButton, rightButton) ->                
@@ -128,10 +132,11 @@
                 return [newImageHeight, newImageWidth]
       
             setImageProperties = (img, path) ->  
-                if path                                
+                if path   
                     img.src = path               
                     img.style.display = 'none'   
-                    img.id = "img_active"                             
+                    img.id = "img_active"  
+
                     img.onload = ->
                         imageHeight = this.height
                         imageWidth = this.width                    
@@ -145,39 +150,88 @@
                         el.style.overflow = 'hidden'                    
                         el.style.left = windowWidth/2+'px'                             
                         el.style.padding = 0+'px'  
-                        
                         img.style.display = 'initial'
-                        addOverlayButtons()
+                        
+                        navigated = binding.value.navigated
+                        
+                        switch navigated
+                            when Number(1)
+                                img.style.left = '-100%'
+                                $('#img_active').animate
+                                    left: '0%'
+                                , 350, null
 
-                        imageBox = el.getBoundingClientRect()
-                            
+                            when Number(2)
+                                img.style.left = '100%'
+                                $('#img_active').animate
+                                    left: '0%'
+                                , 350, null
+
+                        addOverlayButtons() 
+
                         $('.modal').modal('refresh') 
-                    
+                  
+                        canMoveLeft = true
+                        canMoveRight = true
+                   
+                        if (binding.value.position == 0)
+                            canMoveLeft = false
+                        if (binding.value.position == 2)
+                            canMoveRight = false
+                        
+                        img.moveLeft = canMoveLeft
+                        img.moveRight = canMoveRight
+                        
+                        imageBox = el.getBoundingClientRect()
+
+                        inArea = (position, img) ->
+                            imageRect = img.getBoundingClientRect()                                
+                            imageOffset = imageRect.left+imageRect.width/2
+                            inside = false
+
+                            switch position
+                                when "left"
+                                    imageBoxLeftArea = imageBox.left+(Number(imageRect.width/3))                                
+                                    inside = imageOffset < imageBoxLeftArea
+                                when "right"
+                                    imageBoxRightArea = imageBox.right-(Number(imageRect.width/3)) 
+                                    inside = imageOffset > imageBoxRightArea
+                                    
+                            return inside
+
                         $(img).draggable({ 
                             axis: "x",                         
                             appendTo: 'parent',                        
                             scroll: true,
-                        
+                            
+                            drag: (e) =>
+                                if !this.moveLeft                                                                    
+                                    if inArea("right", img)
+                                        $( "#img_active" ).draggable( "option", "revert", true );
+                      
+                                if !this.moveRight
+                                    if inArea("left", img)
+                                        $( "#img_active" ).draggable( "option", "revert", true );
+                            
                             stop: (e) =>                                                                      
-                                imageRect = img.getBoundingClientRect()                                
-                                imageOffset = imageRect.left+imageRect.width/2
-                                imageBoxLeftArea = imageBox.left+(Number(imageRect.width/3))                                
-                                imageBoxRightArea = imageBox.right-(Number(imageRect.width/3))                                 
-                           
-                                if (imageOffset < imageBoxLeftArea)
+                                if inArea("left", img)
+                                    imageRect = img.getBoundingClientRect()  
                                     $('#img_active').animate
                                         left: "-="+Number(imageBox.left+imageRect.right)
-                                    , 350, rightButtonClicked                                                        
-                                
-                                else if (imageOffset > imageBoxRightArea)                                                                                                               
+                                    , 350, rightButtonClicked                                                                                        
+                                   
+                                else if inArea("right", img)                                                                                                               
+                                    imageRect = img.getBoundingClientRect()  
                                     $('#img_active').animate
                                         left: "+="+Number(imageBox.right+imageRect.left)
                                     , 350, leftButtonClicked
+
                                 else
                                    $('#img_active').animate                                    
                                         left: 0+'px'
                                     , 350, null
-                           
+                                
+                                $( "#img_active" ).draggable( "option", "revert", false );
                         })        
 
                 return img
@@ -189,16 +243,13 @@
                 el.removeChild(image_element)
             
             image_element = document.createElement 'img'                                
-            image_element = setImageProperties(image_element,active_image.path)
+            image_element = setImageProperties(image_element,active_image.path)            
             el.appendChild(image_element)
-            
+
             image_element.style.position = 'relative'
             image_element.style.margin = 'auto'            
             image_element.style.left = '0'
             image_element.style.top = '0'
             image_element.style.right = '0'
-            image_element.style.bottom = '0'
-
-            #window.addEventListener "resize", setImageProperties(image_element,active_image.path)             
- 
+            image_element.style.bottom = '0' 
 </script>                
