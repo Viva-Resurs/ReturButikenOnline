ebd6015638ac3ceb89f74a08d2f62424e87258e9
Daniel Ögren <danielogren82@gmail.com>
2017-06-27 12:53:42 +0200

    console.php: Added a command that removes articles after a specified amount of weeks.

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Carbon\Carbon;
+use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +28,28 @@ Artisan::command('articles:clear', function () {
     DB::table('articles')->delete();
     echo "Cleared all articles";
 })->describe('Clear Articles');
+
+/*
+|  Removes articles older than a specified number of weeks 
+*/
+Artisan::command('articles:remove_old {weeks=2}', function($weeks) {    
+    $today = Carbon::now();
+    $old_articles = array();
+    
+    foreach (Article::all() as $article){       
+        if (($article->publish_interval != '') && ($article->active == false)) {
+            $parts = explode('|', $article->publish_interval);
+            if (count($parts) == 2){                
+                $end = Carbon::createFromFormat('Y-m-d H:i:s', trim($parts[1]));        
+                if ($today->subWeeks($weeks)->gt($end))
+                    array_push($old_articles, $article->id);
+            }
+        }
+    }   
+
+    foreach($old_articles as $article_id){
+        $article = Article::find($article_id);
+        $article->delete();
+    }
+   
+})->describe('Removes articles older than 2 months');
