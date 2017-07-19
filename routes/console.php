ca967d640fd8592f53618a001057e4e3f7d6c2f3
Daniel Ögren <danielogren82@gmail.com>
2017-07-19 11:01:12 +0200

    routes/console: Added comments

--- routes/console.php
+++ routes/console.php
@@ -19,19 +19,25 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
 
+/*
+ *  Randomly generates a number of articles.
+ */
 Artisan::command('articles:generate {nr=1}', function ($nr) {
     factory(App\Article::class, (int) $nr)->create();
     echo "Generated $nr articles.";
 })->describe('Generate Articles [nr] - you can specify how many by passing nr');
 
+/*
+ *  Removes all articles.
+ */
 Artisan::command('articles:clear', function () {
     DB::table('articles')->delete();
     echo "Cleared all articles";
 })->describe('Clear Articles');
 
 /*
-|  Removes articles older than a specified number of weeks 
-*/
+ *  Removes articles older than a specified number of weeks 
+ */
 Artisan::command('articles:remove_old {weeks=2}', function($weeks) {    
     $today = Carbon::now();
     $old_articles = array();
